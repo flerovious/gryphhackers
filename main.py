ed19437fd2e86bbfc691635a521809372a694d20
Jacintha Wee <jacinthawee@gmail.com>
2022-05-21 16:50:54 +0800

    update to bot instead of client

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
 
 @client.event
@@ -22,5 +24,11 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 # Would you rather game
+
+
+
+@bot.command(pass_context=True)
+async def wur_game(ctx):
+    await ctx.send()
 
 client.run(TOKEN)
