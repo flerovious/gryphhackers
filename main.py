a7c93c03db15df0cf81fd121d8ef29dc4253c645
alinaleehx <alinaleehx@gmail.com>
2022-05-22 17:38:14 +0800

    Changes made according to review

--- main.py
+++ main.py
@@ -4,21 +4,15 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-#TOKEN = os.getenv('TOKEN')
-token = 'OTc3NTUyODc5NzkzNjAyNTgw.GTEOxr.G7cxfCRwvXfASb3vuBlHX9RtGWYaY1_cFutGZE'
-
+TOKEN = os.getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix='.')
-
 
 @client.event
 async def on_ready():
     print(f'Bot {client.user} is up and running.')
 
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-#client.run(TOKEN)
-
-client.run(token)
+client.run(TOKEN)
