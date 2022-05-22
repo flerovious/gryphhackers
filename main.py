4816639c2dfaac4a596909d1e6b3effd63c7bcf2
alinaleehx <alinaleehx@gmail.com>
2022-05-22 11:55:21 +0800

    Modularize

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+#TOKEN = os.getenv('TOKEN')
+token = 'OTc3NTYzOTMyODUwMDEyMjIx.G3932j.Zh89XDyAqw8zNSCFYhP_8FgESXKB45rXxX86ZE'
 
 client = commands.Bot(command_prefix='.')
 
@@ -18,4 +19,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(TOKEN)
+#client.run(TOKEN)
+
+client.run(token)
