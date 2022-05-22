626ab602103d1809d587bdafb80ec80c3ba98a01
jazzleong <jazz7_leong@yahoo.com.sg>
2022-05-22 12:30:18 +0800

    Add embed message and non-functional reactions

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import requests
 import random
 import html
+from apps.trivia_app import *
 
 
 class Trivia(commands.Cog):
@@ -34,42 +35,40 @@ class Trivia(commands.Cog):
             # should use to log errors
             await ctx.send(f'Trivia fallthrough reached. ctx = {ctx}')
 
-        def get_question():
-            r = requests.get('https://opentdb.com/api.php?amount=1&type=multiple')
-            assert r.status_code == 200
-            data = r.json()['results'][0]
-            return data
+        # Sets colour of embed sidebar based on difficulty
+        def set_difficulty_colour(difficulty):
+            if difficulty == 'easy':
+                return 0x2eff00  # green
+            elif difficulty == 'medium':
+                return 0xdc7633  # orange
+            else:
+                return 0xff0000  # red
 
-        data = get_question()
-        # Assigns variables
-        category, correct_answer, difficulty, incorrect_answers, question, qn_type = data['category'], data[
-            'correct_answer'], data['difficulty'], data['incorrect_answers'], data['question'], data['type']
+        # TODO -- HOW TO SET
+        number_qn = 1
+        difficulty_choice = Difficulty('easy')
 
-        # Sets colour of embed based on difficulty
-        if difficulty == 'easy':
-            colour = 0x2eff00  # green
-        elif difficulty == 'medium':
-            colour = 0xdc7633  # orange
-        else:
-            colour = 0xff0000  # red
+        # Creates instance of trivia and assigns variables for embed
+        trivia_inst = TriviaApp(no_questions=number_qn, difficulty=difficulty_choice)
+        category = trivia_inst.get_category()
+        difficulty = trivia_inst.get_difficulty()
+        colour = set_difficulty_colour(difficulty)
+        question = trivia_inst.get_question()
+        answers = trivia_inst.get_choices()
 
-        # Randomises answers and sets them to a certain multiple choice
-        answer_list = [correct_answer] + incorrect_answers
-        random.shuffle(answer_list)
+        # Sets them to a certain multiple choice
         answer_dict = {}
         for count, choice in enumerate(['A', 'B', 'C', 'D']):
-            answer_dict[choice] = answer_list[count]
+            answer_dict[choice] = answers[count]
 
         # Embed design
         em = discord.Embed(title="Trivia Question \u2753", description=question, color=colour)
         em.add_field(name=f"Category", value=f"{category}", inline=False)
         em.add_field(name="Difficulty", value=f"{difficulty.capitalize()}", inline=False)
-        em.add_field(name="A", value=f"{answer_dict['A']}", inline=True)
-        em.add_field(name="B", value=f"{answer_dict['B']}", inline=True)
-        em.add_field(name="\u200b", value="\u200b", inline=True)
-        em.add_field(name="C", value=f"{answer_dict['C']}", inline=True)
-        em.add_field(name="D", value=f"{answer_dict['D']}", inline=True)
-        em.add_field(name="\u200b", value="\u200b", inline=True)
+        em.add_field(name="A", value=f"{answer_dict['A']}", inline=False)
+        em.add_field(name="B", value=f"{answer_dict['B']}", inline=False)
+        em.add_field(name="C", value=f"{answer_dict['C']}", inline=False)
+        em.add_field(name="D", value=f"{answer_dict['D']}", inline=False)
         react_msg = await ctx.send(embed=em)
         await react_msg.add_reaction('\U0001f1e6')
         await react_msg.add_reaction('\U0001f1e7')
