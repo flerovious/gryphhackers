b04b794c69b4a2b6d293540771879b5129c4f5a6
flerovious <85042815+flerovious@users.noreply.github.com>
2022-05-23 00:01:24 +0800

    Rename commands

--- cogs/memes.py
+++ cogs/memes.py
@@ -6,13 +6,14 @@ from urllib import response
 import requests
 import io
 
+
 class Memes(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    def getmeme(self):
-        response = requests.get('https://meme-api.herokuapp.com/gimme/programmerhumor')
-        json_data = json.loads(response.text)
+    def get_meme(self):
+        res = requests.get('https://meme-api.herokuapp.com/gimme/programmerhumor')
+        json_data = json.loads(res.text)
         meme_url = json_data['url']
         return meme_url
 
@@ -25,17 +26,18 @@ class Memes(commands.Cog):
     @commands.command()
     async def meme(self, ctx):
         print('Getting your meme')
-        meme_url = self.getmeme()
+        meme_url = self.get_meme()
         completed = False
 
-        while(not completed):
-            try: 
+        while not completed:
+            try:
                 meme = urlopen(meme_url).read()
                 im = io.BytesIO(meme)
                 await ctx.send(file=discord.File(im, "funny_meme.png"))
                 completed = True
             except:
                 print("running again")
+
 
 def setup(client):
     client.add_cog(Memes(client))
--- cogs/wur.py
+++ cogs/wur.py
@@ -4,6 +4,7 @@ import json
 from urllib import response
 import requests
 
+
 class WUR(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -15,15 +16,15 @@ class WUR(commands.Cog):
 
     # commands
     @commands.command()
-    async def wur_game(self, ctx):
+    async def wur(self, ctx):
         print('Game in session')
-        response = requests.get('https://would-you-rather-api.abaanshanid.repl.co')
-        json_data = json.loads(response.text)
+        res = requests.get('https://would-you-rather-api.abaanshanid.repl.co')
+        json_data = json.loads(res.text)
         question = json_data['data']
         lightbulb_emoji = '\uD83D\uDCA1'
         embed_qn = discord.Embed(
-            title = f'{lightbulb_emoji} {question}',
-            color = discord.Color.blue()
+            title=f'{lightbulb_emoji} {question}',
+            color=discord.Color.blue()
         )
         await ctx.send(embed=embed_qn)
         # await ctx.send(f'{question}')
